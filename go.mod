04c6dfd4b95d076aed46c38d8a3958d0b9d37012
Jacob Mompean <jacob.mompean@alliantgroup.com>
2026-01-28 16:19:41 -0600

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/noseGbolin/lenslocked
+
+go 1.25.6
---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	// "os"
+)
+
+func handlerFunc(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Welcome to my awesome site!</h1>")
+}
+
+func main() {
+	http.HandleFunc("/", handlerFunc)
+	// fmt.Fprintln(os.Stdout, "Hello World!")
+	fmt.Println("Starting server on :3000...")
+	http.ListenAndServe(":3000", nil)
+}
