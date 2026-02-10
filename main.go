df0b22cd8fc3673a0f7b821b046bf021d525b630
Jacob Mompean <jacob.mompean@alliantgroup.com>
2026-02-10 16:34:33 -0600

    add basic faq page

--- main.go
+++ main.go
@@ -11,7 +11,29 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func contactHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprintf(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:jacobfullstack@proton.me\">jacobfullstack@proton.me</a>.</p>")
+}
+
+func faqHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprintf(w, `<h1>FAQ Page</h1>
+	<ul>
+		<li>
+			<b>Is there a free version?</b>
+			Yes! We offer a free trial for 30 days on any paid plans.
+		</li>
+		<li>
+			<b>What are your support hours?</b>
+			We have support staff answering emails 24/7, though response
+			times may be a bit slower on weekends.
+		</li>
+		<li>
+			<b>How do I contact support?</b>
+			Email us - <a href=\"mailto:jacobfullstack@proton.me\">jacobfullstack@proton.me</a>
+		</li>
+	</ul>
+	`)
 }
 
 type Router struct{}
@@ -22,6 +44,8 @@ func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 			homeHandler(w, r)
 		case "/contact":
 			contactHandler(w, r)
+		case "/faq":
+			faqHandler(w, r)
 		default:
 			w.WriteHeader(http.StatusNotFound)
 			fmt.Fprintf(w, "<h1>404 - Page Not Found</h1><p>The page you are looking for does not exist.</p>")
