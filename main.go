09a1092b5a3f9ff89e6fcc9d9a33e53118332ee6
Jacob Mompean <jacob.mompean@alliantgroup.com>
2026-02-10 16:24:16 -0600

    restore router

--- main.go
+++ main.go
@@ -14,25 +14,23 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:jacobfullstack@proton.me\">jacobfullstack@proton.me</a>.</p>")
 }
 
-// type Router struct{}
+type Router struct{}
 
-// func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-// 	switch r.URL.Path {
-// 		case "/":
-// 			homeHandler(w, r)
-// 		case "/contact":
-// 			contactHandler(w, r)
-// 		default:
-// 			w.WriteHeader(http.StatusNotFound)
-// 			fmt.Fprintf(w, "<h1>404 - Page Not Found</h1><p>The page you are looking for does not exist.</p>")
-// 			// http.Error(w, "404 - Page Not Found", http.StatusNotFound)
-// 	}
-// }
+func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+		case "/":
+			homeHandler(w, r)
+		case "/contact":
+			contactHandler(w, r)
+		default:
+			w.WriteHeader(http.StatusNotFound)
+			fmt.Fprintf(w, "<h1>404 - Page Not Found</h1><p>The page you are looking for does not exist.</p>")
+			// http.Error(w, "404 - Page Not Found", http.StatusNotFound)
+	}
+}
 
 func main() {
-	// var router Router
-	http.HandleFunc("/", homeHandler)
-	http.HandleFunc("/contact", contactHandler)
+	var router Router
 	fmt.Println("Starting server on :3000...")
-	http.ListenAndServe(":3000", nil)
+	http.ListenAndServe(":3000", router)
 }
